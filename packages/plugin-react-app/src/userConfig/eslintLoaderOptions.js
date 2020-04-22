4aa2e0648eea404ecac6b29cf2987135e5d31ab1
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-04-22 11:17:00 +0800

    chore: code optimize

--- packages/plugin-react-app/src/userConfig/eslintLoaderOptions.js
+++ packages/plugin-react-app/src/userConfig/eslintLoaderOptions.js
@@ -1,18 +1,15 @@
 module.exports = (config, eslintLoaderOptions) => {
-  ['jsx', 'tsx'].forEach((rule) => {
-    if (config.module.rules.get(rule)) {
-      config.module
-        .rule(rule)
-        .exclude
-          .add(/node_modules/)
-          .end()
-        .enforce('pre')
-        .use('eslint')
-          .loader('eslint-loader')
-          .tap((options) => ({
-              ...options,
-              ...eslintLoaderOptions
-            }));
-    }
-  });
+  config.module
+    .rule('eslint')
+    .test(/\.(jsx?|tsx?)$/)
+    .exclude
+      .add(/node_modules/)
+      .end()
+    .enforce('pre')
+    .use('eslint')
+      .loader('eslint-loader')
+      .tap((options) => ({
+          ...options,
+          ...eslintLoaderOptions
+        }));
 };
